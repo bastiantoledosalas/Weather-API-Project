d1870d861567a0cb1d64f90aab6ef5dcd5bc2a59
Bastián Joel Toledo Salas <toledosalasbastian@gmail.com>
2026-03-17 19:50:34 +0000

    feat: implement redis cache service

--- Weather API/src/cache/cache.service.ts
+++ Weather API/src/cache/cache.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Injectable()
-export class CacheService {}
+export class CacheService {
+  private client: Redis;
+
+  constructor() {
+    this.client = new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: 6379,
+    });
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.client.get(key);
+  }
+
+  async set(key: string, value: string, ttl: number): Promise<void> {
+    await this.client.set(key, value, 'EX', ttl);
+  }
+}
