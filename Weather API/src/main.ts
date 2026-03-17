b5c1b8e5b35443674e1c9440bb4631ef05f9fe4c
Bastián Joel Toledo Salas <toledosalasbastian@gmail.com>
2026-03-17 20:31:25 +0000

    feat: configure swagger documentation

--- Weather API/src/main.ts
+++ Weather API/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Configurar Swagger
+  const config = new DocumentBuilder()
+  .setTitle('Weather API')
+  .setDescription('API para consultar el clima utilizado cache con Redis')
+  .setVersion('1.0')
+  .addTag('Weather')
+  .build();
+
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api', app, document); // endpoint
+  await app.listen(3000);
 }
 bootstrap();
