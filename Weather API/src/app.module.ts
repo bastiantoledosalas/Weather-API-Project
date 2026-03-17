bf6083c91105a11f45606601143d434e0c4fed22
Bastián Joel Toledo Salas <toledosalasbastian@gmail.com>
2026-03-17 20:39:49 +0000

    fix: update throttler config to latest version syntax

--- Weather API/src/app.module.ts
+++ Weather API/src/app.module.ts
@@ -4,16 +4,32 @@ import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
 import { CacheModule } from './cache/cache.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     WeatherModule, 
     CacheModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+      ttl: 60,   // segundos
+      limit: 10, // max requests
+      },
+    ],
+  }),
     ConfigModule.forRoot({
       isGlobal: true,
     })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    AppService
+  ],
 })
 export class AppModule {}
