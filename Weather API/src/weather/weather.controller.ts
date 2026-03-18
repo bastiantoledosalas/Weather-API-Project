3d05420c5451ddf8ce298a48af616d3c40d22831
Bastián Joel Toledo Salas <toledosalasbastian@gmail.com>
2026-03-18 20:19:21 +0000

    feat: Add DTO to Swagger

--- Weather API/src/weather/weather.controller.ts
+++ Weather API/src/weather/weather.controller.ts
@@ -1,20 +1,37 @@
 import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { WeatherService } from './weather.service';
-import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { WeatherResponseDto } from './dto/weather-response.dto';
 
 @ApiTags('weather')
 @Controller('weather')
+
 export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Get current weather by city' })
+  @ApiOperation({
+     summary: 'Get current weather by city', 
+    })
   @ApiQuery({
     name: 'city',
     required: true,
     example: 'Santiago',
   })
-  async getWeather(@Query('city') city: string) {
+
+  @ApiResponse({
+    status: 200,
+    description: 'Weather data retrieved successfully',
+    type: WeatherResponseDto,
+  })
+  @ApiResponse({
+    status: 502,
+    description: 'Error fetching weather data',
+  })
+
+  async getWeather(
+    @Query() city: string) {
     // 🔒 Validación mínima necesaria
     if (!city || city.trim() === '') {
       throw new BadRequestException('City parameter is required');
